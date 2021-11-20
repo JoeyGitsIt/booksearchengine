1a8620ce1d5ecbfb7f8715e0c4db7c631ed3def7
JoeyGitsIt <josephperri3@gmail.com>
2021-11-20 14:54:02 -0500

    updated models to export Book, started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,38 +1,68 @@
-const { User } = require("../models");
+const { User, Book } = require("../models");
 
 const resolvers = {
   Query: {
-    getSingleUser: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("Check you are logged in");
     },
   },
 
   Mutation: {
-    // check bradleys functionality
-    createUser: async (parent, { ...body }) => {
-      return User.create({ ...body });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    addComment: async (parent, { thoughtId, commentText }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        {
-          $addToSet: { comments: { commentText } },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Check your email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
+    // what all is getting passed into args?
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const book = await Book.create({
+          ...args,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book.bookId } }
+        );
+
+        return User;
+      }
+      throw new AuthenticationError("You are not logged in!");
     },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
-      );
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const book = await Book.findOneAndDelete({
+          bookId: args.bookId,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: book.bookId } }
+        );
+
+        return User;
+      }
+      throw new AuthenticationError("You are not logged in!");
     },
   },
 };
